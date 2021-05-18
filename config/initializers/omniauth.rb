0d0dc7afbff5a9a0482baa78208a0dd396dd876d
Cass Ilangantileke <s3742928@student.rmit.edu.au>
2021-05-18 16:27:22 +1000

    fixed omniauth typos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.user Omniauth::Builder do
-    provider :twitter, Rails.application.credentials.dig(:twitter, api_key), Rails.application.credentials.dig(:twitter, api_secret)
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, Rails.application.credentials.dig(:twitter, :api_key), Rails.application.credentials.dig(:twitter, :api_secret)
 end
