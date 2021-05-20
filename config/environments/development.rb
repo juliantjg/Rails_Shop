8aa65365ac138ecda58d8275099d7d9c7ae9bb15
juliantjg <juliantj88@gmail.com>
2021-05-20 23:08:20 +1000

    heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,16 +60,16 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'rad2021rails@gmail.com',
-    :password             => "Blahblah123",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => 'rad2021rails@gmail.com',
+  #   :password             => "Blahblah123",
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 end
