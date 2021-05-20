b29855c9c6e15927b314947108b2b2c385fb74a5
juliantjg <juliantj88@gmail.com>
2021-05-20 23:15:15 +1000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,20 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'rad2021rails@gmail.com',
+  :password             => "Blahblah123",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
+host = 'hidden-beach-42020.herokuapp.com' #replace with your own url
+config.action_mailer.default_url_options = { host: host }
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
