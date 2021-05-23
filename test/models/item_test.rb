9769c39360f7f6c84df0133ca9043d6292830e1b
juliantjg <juliantj88@gmail.com>
2021-05-23 21:29:47 +1000

    tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = Item.create(name: 'Winter Vibes', price: 30, description: 'Cold and Brilliant start', saveList: false, category: 'men', popularity: 0, colour_filter: "", size_filter: "")
+  end
+
+  test "item should be valid" do
+    assert @item.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+        @user = User.create(username: 'RAD', email: 'rad2021rmit@gmail.com', password: 'Rails2021', password_confirmation: 'Rails2021')
+    end
+
+    test "user should be valid" do
+        assert @user.valid?
+    end
+
+    test "username should not be too long" do
+        @user.username = "x" * 26
+        assert_not @user.valid?
+    end
+
+    test "password should not be too short" do
+        @user.password = "x" * 6 
+        assert_not @user.valid?
+    end
+
+    test "password should be 8-20 characters excluding special characters" do
+        @user.password = "xxxxxxx*"
+        assert_not @user.valid?
+    end
+
+    test "email should have an email format" do
+        @user.email = "test"
+        assert_not @user.valid?
+    end
+
+    test "password should not be too long" do
+        @user.password = "x" * 22
+        assert_not @user.valid?
+    end
 end
